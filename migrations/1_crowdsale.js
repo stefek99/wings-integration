56673fb7bea230e9773349c277a870fd559bf7d2
borispovod <boris@wings.ai>
2018-02-13 23:00:52 +0300

    Deploy scripts for example crowdsale and token

---
+++ migrations/1_crowdsale.js
@@ -0,0 +1,24 @@
+const CustomCrowdsale = artifacts.require('./contracts/example/CustomCrowdsale.sol')
+const CustomTokenExample = artifacts.require('./contracts/example/CustomTokenExample.sol')
+
+module.exports = async (deployer) => {
+  if (process.env.CROWDSALE) {
+    await deployer.deploy(CustomTokenExample)
+    const token = await CustomTokenExample.deployed()
+
+    /*
+     Params for custom crowdsale:
+     uint256 _minimalGoal,
+     uint256 _hardCap,
+     uint256 _tokensPerEthPrice,
+     address _token
+     */
+    await deployer.deploy(CustomCrowdsale, web3.toWei(1000, 'ether'), web3.toWei(50000, 'ether'), 1000, token.address)
+    const crowdsale = await CustomCrowdsale.deployed()
+
+    console.log(`===== Contracts ====`)
+    console.log(`Crowdsale: ${crowdsale.address}`)
+    console.log(`Token: ${token.address}`)
+    console.log(`====================`)
+  }
+};
